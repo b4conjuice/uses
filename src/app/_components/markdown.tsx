ac01a96d8451d44a2ecb4726955dffa73636dcea
Daniel Lopez <dlopez807@gmail.com>
2024-07-03 11:51:15 -0700

    update markdown styles

--- src/app/_components/markdown.tsx
+++ src/app/_components/markdown.tsx
@@ -1,7 +1,7 @@
 export default function Markdown({ content }: { content: string }) {
   return (
     <article
-      className='prose w-full rounded bg-cb-blue p-4 text-cb-white lg:prose-xl prose-headings:text-cb-white prose-a:text-cb-pink hover:prose-a:text-cb-pink/75 prose-code:text-cb-mint'
+      className='prose mx-auto w-full flex-grow rounded bg-cb-blue p-4 text-cb-white shadow lg:prose-xl prose-headings:text-cb-white prose-h1:text-center prose-a:text-cb-pink hover:prose-a:text-cb-pink/75 prose-strong:text-inherit prose-code:text-cb-mint'
       dangerouslySetInnerHTML={{ __html: content }}
     />
   )
